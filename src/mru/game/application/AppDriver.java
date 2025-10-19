8ded6e5949481e3468d53a0d5d5868eec792938c
Fenna Kort <fenna.kort@tutanota.com>
2025-10-18 20:18:51 -0600

    working on #15 added program launch behaviour

--- src/mru/game/application/AppDriver.java
+++ src/mru/game/application/AppDriver.java
@@ -1,13 +1,16 @@
 package mru.game.application;
+import java.io.IOException;
 
+import mru.game.controller.GameManager;
 
 public class AppDriver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		// This is the starting point of the project!
 		// hint - It usually calls method from GameManager class to start the app, so only one or two lines will be written here
 		//related to issue #15, design
+		GameManager game = new GameManager();
 	}
 
 }
